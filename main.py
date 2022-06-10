145b708f133b5f29495ea659ad4d438bd6f16c9a
Kirill Frolovskiy <97293194+FrolovskiyKirill@users.noreply.github.com>
2022-06-10 10:32:34 +0000

    final ver.

--- main.py
+++ main.py
@@ -31,14 +31,20 @@ scissors = '''
 all_choices = [rock, paper, scissors]
 
 your_choice = int(input("Введи '1' для камня, '2' для бумаги и '3' для ножниц.\n"))
-print(f"Твой выбор:\n{all_choices[your_choice - 1]}")
 
-computer_choice = random.randint(1, 3)
-print(f"Выбор компьютера:\n{all_choices[computer_choice - 1]}")
 
-if your_choice == computer_choice:
-  print("Ничья.")
-elif your_choice == 1 and computer_choice == 3 or your_choice == 2 and computer_choice == 1 or your_choice == 3 and computer_choice == 2:
-  print("Ты выиграл!")
+if your_choice < 1 or your_choice > 4:
+  print("Ты ввел неверный номер.")
 else:
-  print("Ты проиграл!")
+ print(f"Твой выбор:\n{all_choices[your_choice - 1]}")
+ computer_choice = random.randint(1, 3)
+ print(f"Выбор компьютера:\n{all_choices[computer_choice - 1]}")
+
+ if your_choice == computer_choice:
+  print("Ничья.")
+ elif your_choice == 1 and computer_choice == 3 or your_choice == 2 and computer_choice == 1 or your_choice == 3 and computer_choice == 2:
+  print("Ты выиграл!")
+ else:
+  print("Ты проиграл!") 
+
+
